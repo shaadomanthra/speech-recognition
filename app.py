f48861f038eb9832d0bfae68414e8b20fab76c6f
Krishna Teja <packetcode@gmail.com>
2020-07-06 13:26:06 +0530

    some changes

--- app.py
+++ app.py
@@ -3,7 +3,10 @@
 # import speech library & webbrowser
 import speech_recognition as sr
 import webbrowser
+import time
 
+
+start = time.time()
 # create recognizer object, load the file and open the audio
 r = sr.Recognizer()
 file = sr.AudioFile('open.wav')
@@ -12,5 +15,8 @@ with file as source:
 
 # use google speech recognition to convert audio to text
 text = r.recognize_google(audio)
+end = time.time()
+elap = (end - start)
 
-print(text)                           
+print(text)
+print("[INFO] Time taken {:.4f} seconds".format(elap))
